e4edcd0341f1bb5a1e448e96490f533b8632ac83
Helvio Pedreschi <helvio88@gmail.com>
2022-11-19 00:33:47 +0000

    Tests 1-11 completed

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,53 @@
-export function syncTrue() {
-  if (true) {
-    return true;
-  } else {
-    return false;
-  }
-}
-  
-export async function asyncTrue() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (true) {
-        resolve(true);
-      } else {
-        resolve(false);
+export class Calculator {
+  sum(input: string = ''){
+
+    // Return 0 for no input
+    if(!input) {
+      return 0;
+    }
+
+    // Delimiter can be a string or array of string. Default value is ","
+    let delimiters: string | string[] = ",";
+    // If a character sequence for delimiter(s) is found, process it
+    if(input.indexOf("//") === 0) {
+      // Get single delimiter or "TI-84" sequence for multiples (format: [delim1][delim2][delim3])
+      delimiters = input.substring(2, input.indexOf('\n'));
+      // If multiple delimiters are detected, parse them properly
+      if(delimiters.indexOf('[') !== -1 && delimiters.indexOf(']') !== -1) {
+        // Transform a "TI-84" string of delimiters into a vallid JSON array
+        delimiters = JSON.parse(delimiters.replaceAll('[', '["').replaceAll(']', '"]').replaceAll('][', ','))
       }
-    }, 1000);
-  });
+      // input becomes the value after the line break that ends the delimiters section
+      input = input.split("\n")[1];
+    }
+
+    // If multiple delimiters, transform them all into line breaks (because of next step)
+    if(delimiters instanceof Array) {
+      delimiters.forEach(delimiter => {
+        input = input.replaceAll(delimiter, '\n')
+      })
+    }
+
+    // Transform all line breaks into first delimiter and split by it
+    const splitValues = input.replaceAll('\n', delimiters[0]).split(delimiters[0])
+    
+    // Math time!
+    let sum = 0;
+    const negatives = []
+    splitValues.forEach(value => {
+      // Only sum positives inputs up to 1000
+      if(+value > 0 && +value < 1000) {
+        sum += +value
+      }
+      // Negatives are not allowed, add them to an exception list
+      else if(+value < 0) {
+        negatives.push(+value)
+      }
+    });
+    // Throw if negatives are found
+    if (negatives.length) {
+      throw new Error(`negatives not allowed: ${negatives.join(', ')}`)
+    }
+    return sum
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,83 @@
 import { expect } from 'chai';
-import { syncTrue, asyncTrue } from '../src/index';
+import { Calculator } from '../src/index';
 
-describe('bar', () => {
-    it('sync function returns true', () => {
-        const result = syncTrue();
-        expect(result).to.be.true;
-    });
+describe('The string calculator', () => {
+    const calculator = new Calculator();
 
-    it('async function returns true', async () => {
-        const result = await asyncTrue();
-        expect(result).to.be.false;
-    });
+    // 1. Given the user input is empty when calculating the sum then it should return zero.
+    describe("when summing with no input", () => {
+        it('should return a zero', () => {
+            expect(calculator.sum()).to.equal(0);            
+        });        
+    })
+    // 2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
+    describe("when a single number is input", () => {
+        it("should return the same number", () => {
+            expect(calculator.sum('3')).to.equal(3);
+        })
+    })
+
+    // 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
+    describe("when summing with two numbers", () => {
+        it("should return the sum of both numbers", () => {
+            expect(calculator.sum("2,3")).to.equal(5);
+        })
+    })
+
+    // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+    describe("when summing 0..n numbers", () => {
+        it("should return the sum of all numbers", () => {
+            expect(calculator.sum("1,2,3")).to.equal(6);
+        })
+    })
+
+    // 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
+    describe("when summing with comma and new line delimiters", () => {
+        it("should return the sum of the numbers", () => {
+            expect(calculator.sum("1,2\n4\n5")).to.equal(12);
+        })
+    })
+
+    // 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
+    describe("when input a custom single-character delimiter", () => {
+        it("should use it to separate the numbers and return the sum of them", () => {
+            expect(calculator.sum('//;\n1;2')).to.equal(3);
+        })
+    })
+    
+    // 7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
+    describe("when attempting to sum with a negative number", () => {
+        it("should throw an exception with the correct message", () => {
+            expect(() => calculator.sum("1,-3,4")).to.throw(`negatives not allowed: -3`);
+        })
+    })
+
+    // 8. Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers).
+    describe("when attempting to sum with multiple negative numbers", () => {
+        it("should throw an exception with the correct message", () => {
+            expect(() => calculator.sum("1,-2,3,-4,5,-6")).to.throw(`negatives not allowed: -2, -4, -6`);
+        })
+    })
+
+    // 9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
+    describe("when attempting to sum with numbers larger than 1000", () => {
+        it("should only sum numbers smaller than 1001", () => {
+            expect(calculator.sum("1001,2")).to.equal(2);
+        })
+    })
+
+    // NOTE: I am not sure that 1**_2_**3 was intended, so I assumed it was meant to be 1***2***3
+    // 10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1**_2_**3” should return 6)
+    describe("when attempting to sum multiple numbers with a multi-char delimiter", () => {
+        it("should return the sum of all numbers", () => {
+            expect(calculator.sum("//[***]\n1***2***3")).to.equal(6);
+        })
+    })
+
+    //11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[\*][%]\n1\*2%3” should return 6)
+    describe("when attempting to sum multiple numbers with multiple delimiters", () => {
+        it("should return the sum of all numbers", () => {
+            expect(calculator.sum("//[\*][%]\n1\*2%3")).to.equal(6);
+        })
+    })
 });
